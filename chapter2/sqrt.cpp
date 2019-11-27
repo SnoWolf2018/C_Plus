60681328f6ca411a15e95cd45e846bc5e8d9f4e2
SnoWolf2018 <SnoWolf2018@gmail.com>
2019-11-27 17:52:33 +0800

    add ch2

---
+++ chapter2/sqrt.cpp
@@ -0,0 +1,17 @@
+// sqrt.cpp --using the sqrt() function
+#include <iostream>
+#include <cmath>    //or math.h
+
+int main()
+{
+    using namespace std;
+    double area;
+    cout << "Enter the floor area, in square feet, of your home:" ;
+    cin >> area;
+    double side;
+    side = sqrt(area);
+    cout << "That's the equivalent of a square " << side
+         <<  " feet to the side." << endl
+         << "How fascinating!" << endl;
+    return 0;
+}
