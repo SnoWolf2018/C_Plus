7f18bd481c515f321364ffa52597afdf5561e5db
SnoWolf2018 <574366448@qq.com>
2020-04-20 09:17:55 +0800

    initial commit

---
+++ ch8/8.11functemp.cpp
@@ -0,0 +1,35 @@
+// funtemp.cpp -- using a function template
+#include <iostream>
+//  function template prototype
+template <typename T> 		//or class T
+void Swap(T &a,T &b);
+int main()
+{
+	using namespace std;
+	int i =10;
+	int j =20;
+	cout<<"i,j ="<<i<<","<<j<<endl;
+	cout<<"Using compiler-generated int	swapper:\n";
+	Swap(i,j);		//generates void Swap(int &,int &)
+	cout<<"Now i,j="<<i<<","<<j<<endl;
+
+	double x=24.5;
+	double y=81.7;
+	cout<<"x,y = "<<x<<","<<y<<endl;
+	cout<<"Using compiler-generated int	swapper:\n";
+	Swap(x,y);		//generates void Swap(int &,int &)
+	cout<<"Now x,y="<<x<<","<<y<<endl;
+
+
+	return 0;
+}
+
+//  function template definition
+template<typename T> 		//or class T
+void Swap(T &a,T &b)
+{
+	T temp;		//temp a variable of type T
+	temp =a;
+	a=b;
+	b=temp;
+}
